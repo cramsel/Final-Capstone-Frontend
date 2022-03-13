23db53aae92b097bd4302001f3fe46407b89b974
cramsel <Luxicorde@gmail.com>
2022-03-13 11:51:26 -0500

    Make file uploads possible as well as the new default instead of URLs

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -4,15 +4,26 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      newPostInfo: {},
+      title: "",
+      description: "",
+      audioUrl: "",
     };
   },
   created: function () {},
   methods: {
+    setFile: function () {
+      if (event.target.files.length > 0) {
+        this.audioUrl = event.target.files[0];
+      }
+    },
     newPost: function () {
-      axios.post("/posts", this.newPostInfo).then((response) => {
-        console.log(response.data);
+      var formData = new FormData();
+      formData.append("title", this.title);
+      formData.append("description", this.description);
+      formData.append("audio_file", this.audioUrl);
 
+      axios.post("/posts", formData).then((response) => {
+        console.log(response.data);
         this.$router.push("/");
       });
     },
@@ -25,11 +36,11 @@ export default {
     <h2>New Post</h2>
     <form v-on:submit.prevent="newPost()">
       Name:
-      <input type="text" v-model="newPostInfo.title" />
+      <input type="text" v-model="title" />
       Description:
-      <input type="text" v-model="newPostInfo.description" />
+      <input type="text" v-model="description" />
       Audio:
-      <input type="text" v-model="newPostInfo.audio_url" />
+      <input type="file" v-on:change="setFile($event)" ref="audioUrl" />
       <input type="Submit" value="Post" />
     </form>
   </div>
